c1be43719286a4b1d9624b56cb3e4a36fcaaf9fd
Umang Giri <umanggiri103@gmail.com>
2026-02-01 09:50:28 +0545

    added footer section

--- js/common.js
+++ js/common.js
@@ -1,29 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const projectSwiperEl = document.querySelector(".projectSwiper");
+  new Swiper(".projectSwiper", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
 
-  if (projectSwiperEl) {
-    new Swiper(projectSwiperEl, {
-      slidesPerView: 4,
-      spaceBetween: 25,
-      loop: true,
-      grabCursor: true,
+    navigation: {
+      nextEl: ".project-swiper-next",
+      prevEl: ".project-swiper-prev",
+    },
 
-      navigation: {
-        nextEl: ".project-next",
-        prevEl: ".project-prev",
-      },
-
-      breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        992: {
-          slidesPerView: 4,
-        },
-      },
-    });
-  }
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 4 },
+    },
+  });
 });
