d8e92ff9489d5ea6371f64d506007a2ca698aab6
Umang Giri <umanggiri103@gmail.com>
2026-02-02 22:27:47 +0545

    made mobile responsive

--- js/common.js
+++ js/common.js
@@ -16,3 +16,33 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   });
 });
+
+document.querySelectorAll(".submenu-toggle").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const menu = this.closest(".mega-item").querySelector(".mega-menu");
+    const isOpen = menu.classList.contains("open");
+
+    document
+      .querySelectorAll(".mega-menu")
+      .forEach((m) => m.classList.remove("open"));
+
+    document
+      .querySelectorAll(".submenu-toggle")
+      .forEach((b) => (b.textContent = "+"));
+
+    if (!isOpen) {
+      menu.classList.add("open");
+      this.textContent = "−";
+    }
+  });
+});
+
+const navbar = document.getElementById("heroNavbar");
+
+navbar.addEventListener("shown.bs.collapse", function () {
+  document.body.classList.add("menu-open");
+});
+
+navbar.addEventListener("hidden.bs.collapse", function () {
+  document.body.classList.remove("menu-open");
+});
